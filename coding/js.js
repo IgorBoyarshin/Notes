54061816776ddfec875ce11117c791894f198781
Igor Boyarshin <igor.boyarshin@gmail.com>
2019-06-23 13:22:42 +0300

    updated

---
+++ coding/js.js
@@ -0,0 +1,9 @@
+npm init --yes
+npm install -D eslint
+node_modules/.vin/eslint -- init
+
+for (key in database) {
+  if ({}.hasOwnProperty.call(database, key)) {
+    distributor.place(database[key]);
+  }
+}
